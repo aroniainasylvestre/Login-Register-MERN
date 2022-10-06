9f850ccccf03f79f0d490d054adeb852ce325a2a
Aroniaina <aroniainasylvestre@gmail.com>
2022-10-06 13:28:40 +0200

    add user routes - model - controllers

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,91 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Generate token
+const generateToken = (id) =>
+    jwt.sign({ _id: id }, process.env.JWT_SECRET, { expiresIn: "60s" });
+
+/**
+ * @description :      Create an account
+ * @Route :              POST   /auth/users/register
+ * @Access :            Public
+ */
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        if (!name)
+            return res.status(400).json({ message: "Entrer your name." });
+
+        if (!email)
+            return res.status(400).json({ message: "Entrer your email." });
+
+        // Check if email is alredy in use
+        const usedEmail = await User.findOne({ email });
+        if (usedEmail)
+            res.status(400).json({ message: "That email is already in use" });
+
+        if (!password)
+            return res.status(400).json({ message: "Entrer your password." });
+
+        // hash user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create user
+        const newUser = {
+            name,
+            email,
+            password: hashedPassword,
+        };
+        const user = await User.create(newUser);
+        if (user) {
+            res.status(201).json({ message: "User registered successfully. Please Sign in now."});
+        } else {
+            res.status(400).json({ message: `Invalide user data`});
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong. Please try again.",
+        });
+    }
+};
+
+/**
+ * @description :      Sign in to an account
+ * @Route :              POST   /auth/users/login
+ * @Access :            Public
+ */
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // find the user
+        const user = await User.findOne({ email });
+        if (!user) res.status(404).json({ message: "User not found." });
+
+        // Verify user password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            res.status(400).json({ message: "Incorrect Password." });
+        }
+
+        // Generate Token
+        const token = generateToken(user._id);
+
+        res.status(200).json({
+            id: user._id,
+            name: user.name,
+            token
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong. Please try again.",
+        });
+    }
+};
+
+
+module.exports = {
+    register,
+    login,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,35 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
 
-// Create express app
+// Create Express app
 const app = express();
+
+// Connect to the database
+const connectDB = require("./config/database.config");
+const URL = process.env.MONGO_URI;
+connectDB(URL);
+
+// Cors
+const corsOption = {
+    origin: process.env.CLIENT_URL,
+};
+app.use(cors(corsOption));
+
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Morgan
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Routes
+const userRoutes = require("./routes/user.routes");
+app.use("/api/auth/users", userRoutes);
 
 // Run the server
 const PORT = process.env.PORT || 8080;
